4cc09a180832ed99718d71b9fdfc0b6f794cfea6
OmegaRus <omega@nimas.li>
2017-07-20 18:03:36 +0300

    updates

--- scripts/sdk_downloader.sh
+++ scripts/sdk_downloader.sh
@@ -4,7 +4,7 @@ wget https://dl.google.com/android/repository/tools_r${ANDROID_SDK_VERSION}-linu
     && unzip tools_r${ANDROID_SDK_VERSION}-linux.zip \
     && rm -f tools_r${ANDROID_SDK_VERSION}-linux.zip \
     && mkdir android-sdk && mv tools android-sdk/tools
-echo y | android update sdk -u -a -t platform-tools \
-    && echo y | android update sdk -u -a -t build-tools-25.0.2 \
-    && echo y | android update sdk -u -a -t android-24 \
-    && echo y | android update sdk -u -a -t sys-img-x86-android-24
+cd android-sdk/tools && echo y | ./android update sdk -u -a -t platform-tools \
+    && echo y | ./android update sdk -u -a -t build-tools-25.0.2 \
+    && echo y | ./android update sdk -u -a -t android-24 \
+    && echo y | ./android update sdk -u -a -t sys-img-x86-android-24
